45edd77b0d6368243e2e47cc863437eac8cc557b
Angelin Lalev <lalev@uni-svishtov.bg>
2023-01-30 15:19:41 +0200

    Initial commit

---
+++ src/main/java/bg/uni_svishtov/demo_students_2023/DemoStudents2023Application.java
@@ -0,0 +1,13 @@
+package bg.uni_svishtov.demo_students_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStudents2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoStudents2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/bg/uni_svishtov/demo_students_2023/DemoStudents2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.uni_svishtov.demo_students_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStudents2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
